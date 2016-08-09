5dd2e12f0da0c0a1a50f10094b5de016ec33dbc9
Maureen Holland <maureen.holland11@gmail.com>
2016-08-09 14:47:47 -0400

    Set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const svg = require('gulp-inject-svg');
+
+gulp.task('watch', function(){
+	gulp.watch('./styles/**/*.scss', ['styles']);
+});
+
+gulp.task('styles', function(){
+	return gulp.src('./styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/styles/'))
+});
+
+gulp.task('svg', function(){
+	return(gulp.src('./**/*.html'))
+		.pipe(injectSvg())
+		// should there be a copy of index.html in the public folder?
+		.pipe(gulp.dest('./public/'));
+});

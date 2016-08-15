c2c1e53484268e3d0dd91cfc62edbb54a414bbc0
Trevor Stone <trevor_stone@me.com>
2016-08-15 11:37:02 -0400

    trying to fit gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ const concat = require('gulp-concat');
 const injectSvg = require('gulp-inject-svg');
 
 gulp.task('watch', function(){
-	gulp.watch('./dev/styles/**/*.scss', ['styles']);
-	gulp.watch('./dev/index.html', ['svg']);
+	gulp.watch('./public/styles/**/*.scss', ['styles']);
+	gulp.watch('./index.html', ['svg']);
 });
 
 gulp.task('styles', function(){
-	return gulp.src('./dev/styles/**/*.scss')
+	return gulp.src('./public/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('./public/styles/'))
 });
 
 gulp.task('svg', function(){
-	return(gulp.src('./dev/*.html'))
+	return(gulp.src('./*.html'))
 		.pipe(injectSvg())
 		// should there be a copy of index.html in the public folder?
 		.pipe(gulp.dest('.'));
